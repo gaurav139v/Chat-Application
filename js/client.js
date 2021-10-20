4aa8ad0aa6cc94c7a3273eb847745aae0a643de7
GAURAV VERMA <gaurav139verma@gmail.com>
2021-10-21 00:14:41 +0530

    Added the chat message new style

--- js/client.js
+++ js/client.js
@@ -6,12 +6,42 @@ const messageInput = document.getElementById('userMessage');
 const messageContainer = document.querySelector('.block');
 const audio = new Audio('./data/tone.mp3');
 
-const append = (message, position)=>{
+// const append = (message, position)=>{
+//     const elem = document.createElement('div');
+//     elem.innerText = message;
+//     elem.classList.add('message');
+//     elem.classList.add(position);
+//     messageContainer.append(elem);
+//     if (position == 'left'){
+//         console.log('audio');
+//         audio.play();
+//     }
+// }
+
+const append = (message, position, name='You')=>{
     const elem = document.createElement('div');
-    elem.innerText = message;
+
+    const elem2 = document.createElement('p');
+    elem2.classList.add('messageContent');
+    elem2.innerText = message;
+    elem.appendChild(elem2);
+
+
+    const elem3 = document.createElement('div');
+    elem3.classList.add('content');
+    elem3.appendChild(elem2);
+
+
+    const elem4 = document.createElement('p');
+    elem4.classList.add('name');
+    elem4.innerText = name;
+    
+    elem.appendChild(elem4);
+    elem.appendChild(elem3);  
     elem.classList.add('message');
     elem.classList.add(position);
     messageContainer.append(elem);
+    
     if (position == 'left'){
         console.log('audio');
         audio.play();
@@ -24,7 +54,7 @@ socket.emit('new-user-joined', name);
 form.addEventListener('submit', (e)=>{
     e.preventDefault();
     const message = messageInput.value;
-    append(`You: ${message}`, 'right');
+    append(`${message}`, 'right');
     socket.emit('send', message);
     messageInput.value = '';
 })
@@ -34,7 +64,7 @@ socket.on('user-joined', name=>{
 })
 
 socket.on('receive', data=>{
-    append(`${data.name}: ${data.message}.`, 'left');
+    append(`${data.message}.`, 'left', `${data.name}`);
 })
 
 socket.on('left', (name)=>{
