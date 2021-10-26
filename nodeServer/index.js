03deea3f09c0ab2c2bc8f9cbe3b90c31f110beff
GAURAV VERMA <gaurav139verma@gmail.com>
2021-10-26 17:01:21 +0530

    Change object to pojo classes

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,27 +1,33 @@
+const User = require('./User.js');
+const Message = require('./Message.js');
+const UsersOnline = require('./UsersOnline.js');
 const io = require('socket.io')(8000);
 
-users = {};
+// users = {};
+users = new UsersOnline();
 
 io.on('connection', socket=>{ 
-    socket.on('new-user-joined', name=>{        
-        users[socket.id] = {'Name': name, 'Color': 'Red'};
-        socket.broadcast.emit('user-joined', users[socket.id]);
- 
+    socket.on('new-user-joined', name=>{
+        var user = new User(socket.id, name);  
+        console.log(user);      
+        // users[socket.id] = user;
+        users.online[socket.id] = user;
+        console.log(users);
+        socket.broadcast.emit('user-joined', user);        
     });
 
     socket.on('send', data=>{
-        data['Name'] = users[socket.id]['Name'];
+        data['Name'] = users.online[socket.id]['Name'];
         data['Id'] = socket.id;
         socket.broadcast.emit('receive', data);  
     })
 
     socket.on('disconnect', ()=>{
-        if (users[socket.id] == undefined){
+        if (users.online[socket.id] == undefined){
             return 0;
         }
-         var data = {'Name': users[socket.id]['Name'], 'Message': 'left the chat.', 'Id': socket.id};
+         var data = {'Name': users.online[socket.id]['Name'], 'Message': 'left the chat.', 'Id': socket.id};
         socket.broadcast.emit('left', data);
-        delete users[socket.id];
-     
+        delete users.online[socket.id];     
     })
 })  
